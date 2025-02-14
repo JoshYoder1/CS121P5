062baa79c67e5f9113998e8daec1a74dc2286263
JoshYoder1 <josh.yoder@bsu.edu>
2025-02-14 20:50:16 +0000

    updated

--- ReadData.cpp
+++ ReadData.cpp
@@ -3,13 +3,35 @@
 #include <sstream>
 
 int main(){
+	// initialize variables
 	std::ifstream file;
 	std::string line;
 	std::stringstream converter;
 	std::stringstream ss;
+	std::string ssCounter;
+	std::string item
+
 	file.open("data.csv");
 	while (getline(file, line)){
 		std::cout << line << std::endl;
+	
+		//clear streams
+		ss.clear();
+		ss.str("");
+		converter.clear();
+		converter("");
+
+		ss.str(line);
+		getline(ss, ssCounter, ",");
+		getline(ss, item);
+
+		//convert
+		converter << ssCounter;
+		converter >> counter;
+
+		//print
+		std::cout << line << item << std::endl;
+
 	}// end while
 	file.close();
 	return 0;
